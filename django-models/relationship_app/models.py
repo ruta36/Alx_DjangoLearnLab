e5417d0af3e18a8c381a3a55f2a039720b0e78dd
ruth <ruthdamtew2086@gmail.com>
2024-08-18 21:26:14 +0400

    add roal

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -26,3 +26,20 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
