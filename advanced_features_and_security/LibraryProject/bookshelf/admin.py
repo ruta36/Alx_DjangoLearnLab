0069427d925dc09756cd974fb6073730c3117b35
ruta36 <ruthdamtew2086@gmail.com>
2024-08-26 00:55:59 +0400

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,7 +8,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
-
+admin.site.register(CustomUser, CustomUserAdmin)
 
 
 
