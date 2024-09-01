970c6871e53649c867a6f064c220ab978c200005
ruth <ruthdamtew2086@gmail.com>
2024-09-01 20:39:50 +0400

    update

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import BookList
+pythonCopy codefrom django.urls import path
+from .views import BookListCreateAPIView
 
+urlpatterns = [
+    path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
+]
 
 urlpatterns = [
      path('admin/', admin.site.urls),  
     path('api/', include('api.urls')),
-    path('api/books', BookList.as_view(), name='book-list'),
+    # path('api/books', BookList.as_view(), name='book-list'),
 ]
