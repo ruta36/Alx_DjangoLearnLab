1f21a7ecb24c091b3281a54a9fa3ea68f2b92978
ruth <ruthdamtew2086@gmail.com>
2024-09-01 20:56:37 +0400

    update

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 from .views import BookList
-pythonCopy codefrom django.urls import path
-from .views import BookListCreateAPIView
+
 
 urlpatterns = [
-    path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
-]
-
-urlpatterns = [
-     path('admin/', admin.site.urls),  
-    path('api/', include('api.urls')),
-    # path('api/books', BookList.as_view(), name='book-list'),
+    # path('admin/', admin.site.urls),  
+    # path('api/', include('api.urls')),
+    path('books', BookList.as_view(), name='book-list'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from .serializers import BookSerializer
 
 
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
