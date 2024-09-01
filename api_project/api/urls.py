c1af348bb9dba32f2e08bf0a66a783fea59fa825
ruth <ruthdamtew2086@gmail.com>
2024-09-01 20:19:39 +0400

    update

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,5 +3,7 @@ from .views import BookList
 
 
 urlpatterns = [
+     path('admin/', admin.site.urls),  
+    path('api/', include('api.urls')),
     path('api/books', BookList.as_view(), name='book-list'),
 ]
