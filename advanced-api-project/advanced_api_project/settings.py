8db2df804ec6aa43b7217a7927ef8d1802539372
ruth <ruthdamtew2086@gmail.com>
2024-09-09 00:52:28 +0400

    done task two

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -101,6 +101,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import TokenAuthentication
 # Create your views here.
+
+
+class BookListView(generics.ListCreateAPIView):
+    """
+    List all books or create a new book.
+    - GET: Retrieve a list of books.
+    - POST: Create a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny] 
+    
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a book.
+    - GET: Retrieve a book by ID.
+    - PUT/PATCH: Update a book by ID.
+    - DELETE: Delete a book by ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  
+
+
+
